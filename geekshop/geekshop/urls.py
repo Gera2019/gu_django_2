b1ec18b9c869018ad915066b69532223f319fb66
Gera2019 <gumanoid2@gmail.com>
2021-11-15 23:53:29 +0300

    lesson 3 completed, .gitignore updated

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -20,14 +20,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    # re_path(r'^admin/', admin.site.urls),
-    # re_path(r'^$', main, name='main'),
-    # re_path(r'^products/', include('mainapp.urls', namespace='products')),
-    # re_path(r'^contacts/', include('contactsapp.urls', namespace='contacts')),
-    # re_path(r'^auth/', include('authapp.urls', namespace='auth')),
-    # re_path(r'^basket/', include('basketapp.urls', namespace='basket')),
-    # re_path(r'^admin_staff/', include('adminapp.urls', namespace='admin_staff')),
-    # path('', include('social_django.urls', namespace='social')),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^$', main, name='main'),
+    path('products/', include('mainapp.urls', namespace='products')),
+    re_path(r'^contacts/', include('contactsapp.urls', namespace='contacts')),
+    re_path(r'^auth/', include('authapp.urls', namespace='auth')),
+    re_path(r'^basket/', include('basketapp.urls', namespace='basket')),
+    re_path(r'^admin_staff/', include('adminapp.urls', namespace='admin_staff')),
+    path('', include('social_django.urls', namespace='social')),
+    re_path(r'^orders/', include('ordersapp.urls', namespace='orders')),
 ]
 
 if settings.DEBUG:
